30831d15056969ed0ec0f05d53f22ff11083054e
IsaacCastelo <isaacpxl8@gmail.com>
2024-05-09 05:25:05 -0700

    Revert "Merge branch 'main' of https://github.com/IsaacCastelo/AppRegalos"
    This reverts commit 8e5d3b89057e991893a8f23d29b8a54c67d4d476, reversing
    changes made to 71c5dc74fdca4016cf51e8cec912b428f2ced708.

--- app/src/main/java/mx/edu/itson/proyectofinal_regalos/ProdDetail.kt
+++ app/src/main/java/mx/edu/itson/proyectofinal_regalos/ProdDetail.kt
@@ -4,18 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
 
 class ProdDetail : AppCompatActivity() {
     private lateinit var producto: Prod
-    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +21,6 @@ class ProdDetail : AppCompatActivity() {
         val prod_image_cell : ImageView = findViewById(R.id.prod_image_cell)
         val prod_price_cell : TextView = findViewById(R.id.prod_price_cell)
         val btnAgregarCarrito : Button = findViewById(R.id.btnAgregarCarrito)
-        database = FirebaseDatabase.getInstance().reference.child("carrito")
 
         if (bundle != null) {
             val imagen = bundle.getInt("imagen")
@@ -39,50 +34,10 @@ class ProdDetail : AppCompatActivity() {
         btnAgregarCarrito.setOnClickListener {
             agregarAlCarrito()
         }
-
-        // MOSTRAR PESTAÑAS ADICIONALES
-        val btnInicio = findViewById<ImageButton>(R.id.Inicio)
-        val btnCatalogo = findViewById<ImageButton>(R.id.Catalogo)
-        val btnOfertas = findViewById<ImageButton>(R.id.Ofertas)
-        val btnCarrito = findViewById<ImageButton>(R.id.Carrito)
-        val btnPerfil = findViewById<ImageButton>(R.id.Perfil)
-
-        btnInicio.setOnClickListener {
-            // Abre la pantalla de inicio al hacer clic en el botón
-            val intent = Intent(this, InicioTienda::class.java)
-            startActivity(intent)
-        }
-
-        btnCatalogo.setOnClickListener {
-            // Abre la pantalla de catálogo al hacer clic en el botón
-            val intent = Intent(this, Catalogo::class.java)
-            startActivity(intent)
-        }
-
-        btnOfertas.setOnClickListener {
-            // Abre la pantalla de ofertas al hacer clic en el botón
-            val intent = Intent(this, Ofertas::class.java)
-            startActivity(intent)
-        }
-
-        btnCarrito.setOnClickListener {
-            // Abre la pantalla de carrito al hacer clic en el botón
-            val intent = Intent(this, Carrito::class.java)
-            startActivity(intent)
-        }
-
-        btnPerfil.setOnClickListener {
-            // Abre la pantalla de perfil al hacer clic en el botón
-            val intent = Intent(this, Perfil::class.java)
-            startActivity(intent)
-        }
     }
 
     private fun agregarAlCarrito() {
-        val carritoId = database.push().key
-        carritoId?.let {
-            database.child(it).setValue(producto)
-        }
+        // TODO: AGREGAR EL PRODUCTO AL CARRITO CORRECTAMENTE
         mostrarMensaje("Producto añadido exitosamente al carrito")
     }
 
--- app/src/main/java/mx/edu/itson/proyectofinal_regalos/oferta.kt
+++
@@ -1,9 +0,0 @@
-package mx.edu.itson.proyectofinal_regalos
-
-data class oferta(
-    val nombre: String,
-    val descripcion: String,
-    val precioOriginal: Double,
-    val precioOferta: Double,
-    val imagen: Int
-)
