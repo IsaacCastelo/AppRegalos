1d3b9ddaa380b497d7da10ab388076cb55f3cbba
eldeividxy <deividsotelo02@gmail.com>
2024-05-09 13:54:47 -0700

    oferta

--- app/src/main/java/mx/edu/itson/proyectofinal_regalos/Ofertas.kt
+++ app/src/main/java/mx/edu/itson/proyectofinal_regalos/Ofertas.kt
@@ -5,14 +5,23 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.GridView
 
 class Ofertas : AppCompatActivity() {
+    private lateinit var ofertas: ArrayList<oferta>
+    private lateinit var gridView: GridView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ofertas)
+
+        ofertas = ArrayList()
+        ofertas.add(oferta("Botana de cumpleaños", "Caja con globo y algunas botanas", 100.0, 80.0, R.drawable.cumplebotanas))
+        ofertas.add(oferta("Caja de cerveza", "Caja especial para regalar en cumpleaños", 200.0, 150.0, R.drawable.cumplecheve))
+
+        // Configurar el GridView
+        gridView = findViewById(R.id.gridView)
+        gridView.adapter = OfertasAdapter(this, ofertas)
 
         val btnInicio = findViewById<ImageButton>(R.id.Inicio)
         val btnCatalogo = findViewById<ImageButton>(R.id.Catalogo)
